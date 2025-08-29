c30a9b519c347559bac3d733242bd25c5dad9f04
SameedIlyas <silyas.bese22seecs@seecs.edu.pk>
2025-08-29 22:50:06 +0500

    Created Dashboard Layout with a Sidebar

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,13 +1,10 @@
-import { AuthGuard } from "@/modules/auth/ui/components/AuthGuard";
-import { OrganizationGuard } from "@/modules/auth/ui/components/OrganizationGuard";
+import { DashboardLayout } from "@/modules/dashboard/ui/layouts/dashboardLayout";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <AuthGuard>
-            <OrganizationGuard>
-                {children}
-            </OrganizationGuard>
-        </AuthGuard>
+        <DashboardLayout>
+            {children}
+        </DashboardLayout>
     )
 }
 
